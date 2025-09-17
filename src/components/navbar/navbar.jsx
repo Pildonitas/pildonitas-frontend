0cc6229492a047414686e8542db7b9855bb44a6a
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-09-17 16:38:47 +0200

    navbar dev

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './navbar.css'
+
+function navbar() {
+  return (
+    <nav class="bg-[#ECF0F1] mb-8">
+        <h1>navbar</h1>
+
+    </nav>
+  )
+}
+
+export default navbar
