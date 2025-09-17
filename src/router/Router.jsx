7ca2f42f8f637f60cc8d5b2ac6579ee00db4cb77
MichelleGel <michellegelves@gmail.com>
2025-09-17 19:07:20 +0200

    Feature/ upload enviroment components and button feature done

---
+++ src/router/Router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+
+const PildonitasRouter = createBrowserRouter([{
+    path:"/",
+    element:<Layout />,
+    children:[
+        {
+            index:true,
+            
+        },
+        {
+            path
+            
+        }
+    ]
+}])
+
+export default PildonitasRouter;
