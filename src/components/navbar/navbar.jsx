ab161d815c9909b9bcabd4eb9ceb7c0a58754adc
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-09-17 17:17:20 +0200

    Hecho Navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -12,23 +12,17 @@ function navbar() {
                         <div class="w-16 h-16 bg-white rounded-full flex items-center justify-center mr-3">
                             <img src={logoPildonitas} alt="Logo_pildonitas" />
                         </div>
-                        <h1 class="text-[#008B7A] text-xl font-semibold">Pildonitas</h1>
+                        <h1 class="text-[#00AEEF] text-xl font-semibold">Pildonitas</h1>
                     </div>
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-                        <a href="#" class="text-white hover:bg-blue-700 px-3 py-2 rounded-md text-sm font-medium">Dashboard</a>
-                        <a href="#" class="text-blue-200 hover:bg-blue-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Proyectos</a>
-                        <a href="#" class="text-blue-200 hover:bg-blue-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Equipo</a>
+                        <a href="#" class="text-[#005F9E] hover:text-white px-3 py-4 rounded-md text-md font-bold">Agregar</a>
+                        <a href="#" class="text-[#005F9E] hover:text-white px-3 py-4 rounded-md text-md font-bold">Historial</a>
+                        <a href="#" class="text-[#005F9E] hover:text-white px-3 py-4 rounded-md text-sm font-bold">Perfil</a>
                     </div>
                 </div>
-                <div class="hidden md:block">
-                    <div class="ml-4 flex items-center md:ml-6">
-                        <button class="bg-blue-800 hover:bg-blue-900 text-white px-4 py-2 rounded-lg text-sm font-medium transition duration-200">
-                            Iniciar Sesión
-                        </button>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </nav>
