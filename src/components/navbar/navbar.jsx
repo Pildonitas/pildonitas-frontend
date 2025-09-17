b40f95243882e21907b017c2f5094fd467a0cfb3
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-09-17 17:35:54 +0200

    Navbar hecho hecho

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,17 +6,17 @@ function navbar() {
   return (
     <nav class="bg-[#ECF0F1] mb-8 rounded-md">
         <div class="max-w-7xl m-3 p-4">
-            <div class="flex justify-between items-center h-16">
+            <div class="flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0 min-h-16">
                 <div class="flex items-center">
                     <div class="flex-shrink-0 flex items-center">
                         <div class="w-16 h-16 bg-white rounded-full flex items-center justify-center mr-3">
                             <img src={logoPildonitas} alt="Logo_pildonitas" />
                         </div>
-                        <h1 class="text-[#00AEEF] text-xl font-semibold">Pildonitas</h1>
+                        <h1 class="text-[#00AEEF] text-l font-semibold">Pildonitas</h1>
                     </div>
                 </div>
-                <div class="hidden md:block">
-                    <div class="ml-10 flex items-baseline space-x-4">
+                <div class="block">
+                    <div class="flex flex-col md:flex-row md:items-baseline space-y-2 md:space-y-0 md:space-x-4">
                         <a href="#" class="text-[#005F9E] hover:text-white px-3 py-4 rounded-md text-md font-bold">Agregar</a>
                         <a href="#" class="text-[#005F9E] hover:text-white px-3 py-4 rounded-md text-md font-bold">Historial</a>
                         <a href="#" class="text-[#005F9E] hover:text-white px-3 py-4 rounded-md text-sm font-bold">Perfil</a>
