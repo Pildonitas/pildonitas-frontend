a04d6cdeac84f9cc256916ebee217ce6eff211b4
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-09-18 10:27:09 +0200

    Cambiado el logo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,12 +5,12 @@ import logoPildonitas from '../../assets/logo_pildonitas.png'
 function navbar() {
   return (
     <nav class="bg-[#ECF0F1] mb-8 rounded-md">
-        <div class="max-w-7xl m-15 p-10">
-            <div class="flex flex-col md:flex-row md:justify-between md:items-center space-y-4 md:space-y-0 min-h-16">
+        <div class="max-w-7xl m-15 p-5">
+            <div class="flex flex-col md:flex-row md:justify-evenly md:items-center space-y-4 md:space-y-0 min-h-16">
                 <div class="flex items-center">
                     <div class="flex-shrink-0 flex items-center">
-                        <div class="w-16 h-16 bg-white rounded-full flex items-center justify-center mr-3">
-                            <img src={logoPildonitas} alt="Logo_pildonitas" />
+                        <div class="w-20 h-20 bg-[#CEE3DB] rounded-full flex items-center justify-center mr-3">
+                            <img src={logoPildonitas} alt="Logo_pildonitas" class="w-15 h-14"/>
                         </div>
                         <h1 class="text-[#00AEEF] text-l font-semibold">Pildonitas</h1>
                     </div>
